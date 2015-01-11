164764a0688712a27f2e01621d268eadab0f9b62
James Dennes <jdennes@gmail.com>
2015-01-11 16:02:32 +0100

    Add example using 2fa_disabled filter

---
+++ export-all-members-with-2fa-disabled.rb
@@ -0,0 +1,16 @@
+# Set OCTOKIT_ACCESS_TOKEN to authenticate
+
+require "octokit"
+
+Octokit.auto_paginate = true
+members = Octokit.org_members "github", :filter => "2fa_disabled"
+
+# Then, for example:
+
+require "csv"
+
+CSV.open("export-2fa-disabled.csv", "wb") do |csv|
+  members.each do |m|
+    csv << [m[:id], m[:login]] # etc
+  end
+end
