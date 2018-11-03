f349a554f9862233dd7a692f400e1db5bd7cca45
Petros Kolyvas <petros@krispii.com>
2018-11-03 11:53:38 -0400

    Scrape tool v1

--- scrape.sh
+++ scrape.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+echo "Scraping for Membership List..."
 bundle exec ruby list-org-members.rb
+echo "Done."
+echo ""
+echo "Scraping for Organization Teams..."
+bundle exec ruby list-org-teams.rb
+echo "Done."
+echo ""
+echo "Scraping for Organization Repositorries..."
 bundle exec ruby list-org-repos.rb
-buddle exec ruby list-org-teams.rb
+echo "Done."
+echo ""
+echo "Scraping for Organization Team Membership..."
 bundle exec ruby list-org-teams-members.rb
+echo "Done."
+echo "Have a great day."
